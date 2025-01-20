0a0f9bd1e2ec95c353406b95ea6b7de4e9e38f9f
Ludmila Lopez <ludmit@outlook.com>
2025-01-19 21:40:05 -0300

    feat: :boom: new logic implemented on transform

--- src/transform.rs
+++ src/transform.rs
@@ -2,26 +2,26 @@
 use ndarray::Array2;
 use image::GenericImageView;
 
+
 pub struct ATrousTransform {
-    input: Array2<f32>, // `Array2<f32>` is a 2D array where each value is of type `f32`. This will hold our pixel data for input image.
-    levels: usize, // The number of levels or scales to decompose the image into
-    current_level: usize, // Current level that we need to generate. This holds the state of our iterator.
-    width: usize, // Width of input image
-    height: usize, // Height of input image
+    input: Array2<f32>, // 2D array to hold pixel data for the input image.
+    levels: usize, // Number of levels or scales to decompose the image into.
+    current_level: usize, // Current level being generated in the iterator.
+    width: usize, // Width of the input image.
+    height: usize, // Height of the input image.
 }
 
-
 impl ATrousTransform {
+    /// Create a new ATrousTransform instance.
     pub fn new(input: &image::DynamicImage, levels: usize) -> Self {
         let (width, height) = input.dimensions();
         let (width, height) = (width as usize, height as usize);
 
-        // Create a new 2D array with proper size for each dimension to hold all of our input's pixel data. Method `zeros` takes a "shape" parameter, which is a tuple of (rows_count, columns_count).
+        // Create a new 2D array to hold all pixel data of the input image.
         let mut data = Array2::<f32>::zeros((height, width));
 
-        // Convert the image to be a grayscale image where each pixel value is of type `f32`. Loop over all pixels in the input image along with its 2D location.
+        // Convert the image to grayscale and populate the array with pixel values.
         for (x, y, pixel) in input.to_luma32f().enumerate_pixels() {
-            // Put the pixel value at appropriate location in our data array. The `[[]]` syntax is used to provide a 2-dimensional index such as `[[row_index, col_index]]`
             data[[y as usize, x as usize]] = pixel.0[0];
         }
 
@@ -30,66 +30,90 @@ impl ATrousTransform {
             levels,
             current_level: 0,
             width,
-            height
+            height,
+        }
+    }
+
+    /// Compute the convoluted pixel value for a given coordinate and distance.
+    pub fn compute_convoluted_pixel(&self, distance: usize, coord: [usize; 2]) -> f32 {
+        let (x, y) = (coord[0], coord[1]);
+        let (width, height) = (self.width, self.height);
+
+        // Define a simple 3x3 kernel for convolution.
+        let kernel: [[f32; 3]; 3] = [
+            [0.1, 0.8, 0.1],
+            [0.8, 1.0, 0.8],
+            [0.1, 0.8, 0.1],
+        ];
+
+        let mut sum = 0.0;
+        let mut weight = 0.0;
+
+        // Apply convolution over the kernel.
+        for ky in 0..3 {
+            for kx in 0..3 {
+                let nx = x as isize + (kx as isize - 1) * distance as isize;
+                let ny = y as isize + (ky as isize - 1) * distance as isize;
+
+                // Ensure the coordinates are within the image bounds.
+                if nx >= 0 && nx < width as isize && ny >= 0 && ny < height as isize {
+                    let pixel_value = self.input[[ny as usize, nx as usize]];
+                    let kernel_value = kernel[ky][kx];
+
+                    sum += pixel_value * kernel_value;
+                    weight += kernel_value;
+                }
+            }
+        }
+
+        // Normalize the pixel value by the total weight of the kernel.
+        if weight > 0.0 {
+            sum / weight
+        } else {
+            0.0
         }
     }
 }
 
 impl Iterator for ATrousTransform {
-    // Our output is an image as well as the current level for each
-    // iteration. The current level is an `Option` to represent the
-    // final residue layer after the intermediary layers have been
-    // generated.
-    type Item = (Array2::<f32>, Option<usize>);
+    type Item = (Array2<f32>, Option<usize>);
 
     fn next(&mut self) -> Option<Self::Item> {
         let pixel_scale = self.current_level;
         self.current_level += 1;
 
-        // We've already generated all the layers. Return None to 
-        // exit the iterator.
+        // Exit the iterator if all layers have been generated.
         if pixel_scale > self.levels {
             return None;
         }
 
-        // We've generated all intermediary layers, return the 
-        // residue layer.
+        // Return the residue layer for the final iteration.
         if pixel_scale == self.levels {
-            return Some((self.input.clone(), None))
+            return Some((self.input.clone(), None));
         }
 
         let (width, height) = (self.width, self.height);
 
-        // Distance between adjacent pixels for convolution (also 
-        // referred to as size of "hole").
+        // Compute the distance between adjacent pixels for this layer.
         let distance = 2_usize.pow(pixel_scale as u32);
 
-        // Create new buffer to hold the computed data for this layer.
+        // Create a new buffer to hold the computed layer data.
         let mut current_data = Array2::<f32>::zeros((height, width));
 
-        // Iterate over each pixel location in the 2D image
+        // Populate the buffer by computing convoluted values for each pixel.
         for x in 0..width {
             for y in 0..height {
-                // Set the current pixel in current layer to
-                // the result of convolution on the current
-                // pixel in input data.
-                current_data[[y, x]] = self.compute_convoluted_pixel(
-                    distance, 
-                    [x, y]
-                );
+                current_data[[y, x]] = self.compute_convoluted_pixel(distance, [x, y]);
             }
         }
 
-        // Create current layer by subtracting currently computed pixels 
-        // from previous layer
+        // Calculate the final layer data by subtracting the computed layer from the input.
         let final_data = self.input.clone() - &current_data;
 
-        // Set the input layer to equal the current computed layer so 
-        // that it can be used as the "previous layer" in next iteration.
-        // This is also our residue data for each layer.
+        // Update the input layer for the next iteration.
         self.input = current_data;
 
-        // Return the current layer data as well as current level information.
+        // Return the current layer data and its scale level.
         Some((final_data, Some(self.current_level)))
     }
 }
